6928514352c0a3ef31ca1b324ec48a965c314501
Rafael Medeiros <rafaelfufcg@gmail.com>
2026-07-02 23:13:59 -0300

    Imprimindo nome, ano e nota do filme

--- primeiroProjeto/src/Main.java
+++ primeiroProjeto/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Olá Mundo");
+        System.out.println("Plataforma de Filmes");
+
+        int anoDoFilme = 2022;
+        double notaDoFilme = 9.1;
+        String nomeDoFilme = "Madagascar";
+
+        System.out.println("Filme: " + nomeDoFilme);
+        System.out.println("Ano: " + anoDoFilme);
+        System.out.println("Nota: " + notaDoFilme);
     }
+
+
+
+
 }
